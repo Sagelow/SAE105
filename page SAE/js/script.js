3ab67e120fb50f74941ddf9ec3733a64d37dca34
Sagelow <155046911+Sagelow@users.noreply.github.com>
2025-12-01 11:57:29 +0100

    Add files via upload

---
+++ page SAE/js/script.js
@@ -0,0 +1,10 @@
+let cles=Object.keys(SAE);
+console.log(cles[0])
+let affichage = ""
+
+for (let i=0; i<cles.length; i++){
+    affichage+='<a href="">'+cles[i]+'</a>'
+    console.log(cles[affichage])
+}
+
+document.querySelector("div").innerHTML=affichage
