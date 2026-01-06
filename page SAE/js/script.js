2710e1eba02e19b946848a04c1910ce366ed8020
Sagelow <155046911+Sagelow@users.noreply.github.com>
2026-01-06 17:09:17 +0100

    Add files via upload

--- page SAE/js/script.js
+++ page SAE/js/script.js
@@ -3,8 +3,13 @@ console.log(cles[0])
 let affichage = ""
 
 for (let i=0; i<cles.length; i++){
-    affichage+='<a href="info/information_SAE.html" class="plus" target="_blank">'+cles[i]+'</a>'
+    affichage+='<a href="info/information_SAE.html?SAE='+cles[i]+'" class="plus">'+cles[i]+'</a>'
     console.log(cles[affichage])
 }
 
 document.querySelector(".result").innerHTML=affichage
+
+let param = new URLSearchParams(location.search);
+let boisson = param.get('boisson');
+let plat = param.get('plat');
+document.querySelector(".resultat").innerHTML+="<br>Votre repas : " + plat + " et " + boisson;
